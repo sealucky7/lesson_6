3fbed7d3b73e26eefc1c6c33bcbf663c948fc9f0
sealucky7 <sea.yastreb@gmail.com>
2015-09-29 12:25:49 +0300

    first commit

--- index.php
+++ index.php
@@ -3,19 +3,19 @@
 session_start();
 
 $location_id = array(641780 => 'Новосибирск', 641490 => 'Барабинск', 641510=>'Бердск', 641600=>'Искитим', 641630=>'Колывань', 641680=>'Краснообск', 641710=>'Куйбышев', 641760=>'Мошково', 641790=>'Обь', 641800=>'Ордынское', 641970=>'Черепаново');
-$category_id_1 = array(9 => 'Автомобили с пробегом', 109 => 'Новые автомобили', 14 => 'Мотоциклы и мототехника', 81 => 'Грузовики и спецтехника', 11 => 'Водный транспорт', 10 => 'Запчасти и аксессуары' );
-$category_id_2 = array(24 => 'Квартиры', 23 => 'Комнаты', 25 => 'Дома, дачи, коттеджи', 26 => 'Земельные участки', 85 => 'Гаражи и машиноместа', 42 => 'Коммерческая недвижимость', 86 => 'Недвижимость за рубежом');
-$category_id_3 = array( 111 => 'Вакансии (поиск сотрудников)', 112 => 'Резюме (поиск работы)');
-$category_id_4 = array( 114 => 'Предложения услуг', 115 => 'Запросы на услуги');
-$category_id_5 = array( 27 => 'Одежда, обувь, аксессуары', 29 => 'Детская одежда и обувь', 30 => 'Товары для детей и игрушки', 28 => 'Часы и украшения', 88 => 'Красота и здоровье');
-$category_id_6 = array( 21 => 'Бытовая техника', 20 => 'Мебель и интерьер', 87 => 'Посуда и товары для кухни', 82 => 'Продукты питания', 19 => 'Ремонт и строительство', 106 => 'Растения' );
-$category_id_7 = array( 32 => 'Аудио и видео', 97 => 'Игры, приставки и программы', 31 => 'Настольные компьютеры', 98 => 'Ноутбуки', 99 => 'Оргтехника и расходники', 96 => 'Планшеты и электронные книги', 84 => 'Телефоны', 101 => 'Товары для компьютера', 105 => 'Фототехника' );
-$category_id_8 = array( 33 => 'Билеты и путешествия', 34 => 'Велосипеды', 83 => 'Книги и журналы', 36 => 'Коллекционирование', 38 => 'Музыкальные инструменты', 102 => 'Охота и рыбалка', 39 => 'Спорт и отдых', 103 => 'Знакомства' );
-$category_id_9 = array( 89 => 'Собаки', 90 => 'Кошки', 91 => 'Птицы', 92 => 'Аквариум', 93 => 'Другие животные', 94 => 'Товары для животных' );
-$category_id_10 = array( 116 => 'Готовый бизнес', 40 => 'Оборудование для бизнеса');
-
-
-
+$categories = array(
+    'Транспорт'=> array(9 => 'Автомобили с пробегом', 109 => 'Новые автомобили', 14 => 'Мотоциклы и мототехника', 81 => 'Грузовики и спецтехника', 11 => 'Водный транспорт', 10 => 'Запчасти и аксессуары' ),
+    'Недвижимость'=> array(24 => 'Квартиры', 23 => 'Комнаты', 25 => 'Дома, дачи, коттеджи', 26 => 'Земельные участки', 85 => 'Гаражи и машиноместа', 42 => 'Коммерческая недвижимость', 86 => 'Недвижимость за рубежом'),
+    'Работа'=> array( 111 => 'Вакансии (поиск сотрудников)', 112 => 'Резюме (поиск работы)'),
+    'Услуги'=> array( 114 => 'Предложения услуг', 115 => 'Запросы на услуги'),
+    'Личные вещи'=> array( 27 => 'Одежда, обувь, аксессуары', 29 => 'Детская одежда и обувь', 30 => 'Товары для детей и игрушки', 28 => 'Часы и украшения', 88 => 'Красота и здоровье'),
+    'Для дома и дачи'=> array( 21 => 'Бытовая техника', 20 => 'Мебель и интерьер', 87 => 'Посуда и товары для кухни', 82 => 'Продукты питания', 19 => 'Ремонт и строительство', 106 => 'Растения' ),
+    'Бытовая электроника'=> array( 32 => 'Аудио и видео', 97 => 'Игры, приставки и программы', 31 => 'Настольные компьютеры', 98 => 'Ноутбуки', 99 => 'Оргтехника и расходники', 96 => 'Планшеты и электронные книги', 84 => 'Телефоны', 101 => 'Товары для компьютера', 105 => 'Фототехника' ),
+    'Хобби и отдых'=> array( 33 => 'Билеты и путешествия', 34 => 'Велосипеды', 83 => 'Книги и журналы', 36 => 'Коллекционирование', 38 => 'Музыкальные инструменты', 102 => 'Охота и рыбалка', 39 => 'Спорт и отдых', 103 => 'Знакомства' ),
+    'Животные'=> array( 89 => 'Собаки', 90 => 'Кошки', 91 => 'Птицы', 92 => 'Аквариум', 93 => 'Другие животные', 94 => 'Товары для животных' ),
+    'Для бизнеса'=> array( 116 => 'Готовый бизнес', 40 => 'Оборудование для бизнеса')
+);?>
+<?php
 // Переносим данные из $_POST в $_SESSION	
 if (!isset($_SESSION['id_ad'])){
 	$_SESSION['id_ad'] = 0;
@@ -126,86 +126,21 @@ if (isset($_SESSION['show'])){
 				<td>
 					<select title="Выберите категорию объявления" name="category_id"  required>
 						<option value="">-- Выберите категорию --</option>
-							<optgroup label="Транспорт">	
-								<?php
-								foreach ($category_id_1 as $id => $category) {?>
-								<option <?php echo (isset($change_id) && $changeAd['category_id']==$id) ? 'selected=""' : '';?> value="<?php echo $id;?>"><?php echo $category;?></option>
-								<?php
+						<?php
+								foreach ($categories as $id => $category) {
+                                                                ?>	
+                                                <optgroup label="<?=$id?>">
+							<?php foreach ($category as $key => $value){
+                                                            ?>
+                                                    <option <?php echo (isset($change_id) && $changeAd['category_id']==$key) ? 'selected=""' : '';?> value="<?php echo $key;?>">  
+                                                                <?php echo $value;?>
+                                                    </option>
+								<?php }?>
+							</optgroup>
+                                                        <?php
 								}
 								?>
-							</optgroup>
-							<optgroup label="Недвижимость">
-								<?php
-								foreach ($category_id_2 as $id => $category) {?>
-								<option <?php echo (isset($change_id) && $changeAd['category_id']==$id) ? 'selected=""' : '';?> value="<?php echo $id;?>"><?php echo $category;?></option>
-								<?php
-								}
-								?>
-							</optgroup>
-							<optgroup label="Работа">
-								<?php
-								foreach ($category_id_3 as $id => $category) {?>
-								<option <?php echo (isset($change_id) && $changeAd['category_id']==$id) ? 'selected=""' : '';?> value="<?php echo $id;?>"><?php echo $category;?></option>
-								<?php
-								}
-								?>
-							</optgroup>
-							<optgroup label="Услуги">
-								<?php
-								foreach ($category_id_4 as $id => $category) {?>
-								<option <?php echo (isset($change_id) && $changeAd['category_id']==$id) ? 'selected=""' : '';?> value="<?php echo $id;?>"><?php echo $category;?></option>
-								<?php
-								}
-								?>
-							</optgroup>
-							<optgroup label="Личные вещи">
-								<?php
-								foreach ($category_id_5 as $id => $category) {?>
-								<option <?php echo (isset($change_id) && $changeAd['category_id']==$id) ? 'selected=""' : '';?> value="<?php echo $id;?>"><?php echo $category;?></option>
-								<?php
-								}
-								?>
-							</optgroup>
-							<optgroup label="Для дома и дачи">
-								<?php
-								foreach ($category_id_6 as $id => $category) {?>
-								<option <?php echo (isset($change_id) && $changeAd['category_id']==$id) ? 'selected=""' : '';?> value="<?php echo $id;?>"><?php echo $category;?></option>
-								<?php
-								}
-								?>
-							</optgroup>
-							<optgroup label="Бытовая электроника">
-								<?php
-								foreach ($category_id_7 as $id => $category) {?>
-								<option <?php echo (isset($change_id) && $changeAd['category_id']==$id) ? 'selected=""' : '';?> value="<?php echo $id;?>"><?php echo $category;?></option>
-								<?php
-								}
-								?>
-							</optgroup>
-							<optgroup label="Хобби и отдых">
-								<?php
-								foreach ($category_id_8 as $id => $category) {?>
-								<option <?php echo (isset($change_id) && $changeAd['category_id']==$id) ? 'selected=""' : '';?> value="<?php echo $id;?>"><?php echo $category;?></option>
-								<?php
-								}
-								?>
-							</optgroup>
-							<optgroup label="Животные">
-								<?php
-								foreach ($category_id_9 as $id => $category) {?>
-								<option <?php echo (isset($change_id) && $changeAd['category_id']==$id) ? 'selected=""' : '';?> value="<?php echo $id;?>"><?php echo $category;?></option>
-								<?php
-								}
-								?>
-							</optgroup>
-							<optgroup label="Для бизнеса">
-								<?php
-								foreach ($category_id_10 as $id => $category) {?>
-								<option <?php echo (isset($change_id) && $changeAd['category_id']==$id) ? 'selected=""' : '';?> value="<?php echo $id;?>"><?php echo $category;?></option>
-								<?php
-								}
-								?>
-							</optgroup>
+
 					</select>
 				</td>	
 			</tr>
@@ -243,4 +178,3 @@ if (isset($_SESSION['bd'])){
 }
 
 ?>
-<pre><?php print_r($_SESSION);?></pre>
